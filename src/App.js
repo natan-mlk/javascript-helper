7a279466c8601101f1d07f5751d0002945a74534
natan-mlk <adam.milko@gmail.com>
2017-09-03 19:04:06 +0200

    some() - example working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Test1 from './some'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        const array1 = [3, 10, 18, 20];
+        const array2 = [3, 10, 15, 11];
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <h2>Welcome to React</h2>
+                </div>
+
+                <Test1 buttonName=".some()" check={array1}/>
+                <Test1 buttonName=".some()" check={array2}/>
+                <p id="demo" className="hide">what will it be?</p>
+
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/some.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+
+class Test1 extends React.Component {
+
+    render () {
+
+        const propsedArray = this.props.check;
+
+        function checkAdult(arg) {
+            return arg >= 18;
+        }
+
+        return (
+            <div>
+                <p>var numbers = [{propsedArray.map((arg) => arg + ", ")}]</p>
+                <span>numbers <button onClick={function myFunction() {
+                    document.getElementById("demo").innerHTML = propsedArray.some(checkAdult);
+                }}>{this.props.buttonName}</button></span>
+
+            </div>
+        )
+    }
+}
+
+export default Test1
+
