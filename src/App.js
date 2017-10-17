cd1597c22affa54ac0177584e0d100e8f69dd8f4
amilko <amilkolpp>
2017-10-17 14:45:32 +0200

    new folders structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {BrowserRouter as Router, Route, NavLink,} from 'react-router-dom';
 
-import './App.css';
+
 import './styles/styles.css';
 
 import Home from './components/home'
